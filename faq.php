2794c08647b1c72b3a507f96d7f401962bd349b8
olbja002 <112383623+olbja002@users.noreply.github.com>
2023-06-03 16:13:28 +0200

    faq

--- faq.php
+++ faq.php
@@ -12,5 +12,18 @@
     <a href="explore.php">Explore</a>
     <a href="admin.php">Admin</a>
     <a href="faq.php">FAQ</a>
+
+    <h2>Hva er nettsiden?</h2><br>
+    <p>Dette er en nettside hvor du kan se på kunst og kjøpe kunst</p><br><br>
+
+    <h2>Jeg får ikke logget inn</h2><br>
+    <p>Har du ikke bruker må du først registrere deg.</p><br>
+    <p>Mener du at du allerede har registrert deg anbefaler vi at du leser feilmeldingen.</p><br>
+    <p>kommer du fortsatt ikke inn kan du sende en mail til hockeyole3@gmail.com</p><br><br>
+
+    <h2>Hvordan registrerer jeg meg?</h2><br>
+    <p>Du kan registrere deg på registreringssiden</p><br><br>
+
+    <h1>Ytterligere spørsmål kan sendes på mail til hockeyole3@gmail.com</h1><br><br>
 </body>
 </html>
